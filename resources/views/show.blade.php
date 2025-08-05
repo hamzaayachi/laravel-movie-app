089772a5cf10fc83e5a30b2d843139a519465545
hamzaayachi <hamza.ayachi.pro@gmail.com>
2025-08-05 19:01:24 +0100

    build movie details

---
+++ resources/views/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('content')
+
+   <div class="movie-info border-b  border-gray-800">
+    <div class="container mx-auto px-4 py-16 flex" >
+        <img src="/img/parasite.jpg" alt="parasite" class="w-96" style="width: 24rem" >
+        <div class="ml-24">
+            <h2 class="text-4xl mt-4 md:mt-0 font-semibold" >parasite (2019)</h2>
+        </div>
+    </div>
+
+   </div> <!-- end movie enfo-->
+
+@endsection
