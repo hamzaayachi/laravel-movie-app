1b440464b02bd9c21fdecdf724829468872562d3
hamzaayachi <hamza.ayachi.pro@gmail.com>
2025-09-05 19:01:04 +0100

    create search imput condition and create poster movie when i search

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -5,17 +5,42 @@
                  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                  </svg>
                 </div>
+
+                @if (strlen($search) >= 2 ) 
+                    
+                
                 <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
+
+                    @if ($searchResults->count() > 0) <!-- this condition for display result search movie -->
+                        
+                    
                     <ul>
 
                         @foreach ($searchResults as $result )
                             
                         
                         <li class="border-b border-gray-700" >
-                            <a href="{{ route('movies.show', $result['id']) }}" class="block hover:bg-fray-700 px-3 py-4" >{{ $result['title'] }}</a>
+                            <a href="{{ route('movies.show', $result['id']) }}" class="block hover:bg-fray-700 px-3 py-3 flex items-center" >
+                                @if ($result['poster_path']) <!-- this condition for the wmovie without poster img -->
+                                
+                                <img src="https://image.tmdb.org/t/p/w92{{ $result['poster_path'] }}" alt="poster" class="w-8">
+
+                                @else 
+                                <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+
+                                @endif
+                               <span class="ml-4"> {{ $result['title'] }}</span>
+                            </a>
                         </li>
 
                         @endforeach
                     </ul>
+
+                    @else 
+                    <div class="px-3 py-3" >No Results for "{{ $search }}" </div>
+
+                    @endif
                 </div>
+
+                @endif
   </div>
